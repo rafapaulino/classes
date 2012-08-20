a8b4653646062d1e2c6e5410e972f0b27347de67
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-08-20 10:13:22 -0300

    Initial Commit

---
+++ classesTests/classesTests.h
@@ -0,0 +1,13 @@
+//
+//  classesTests.h
+//  classesTests
+//
+//  Created by Rafael Brigagão Paulino on 20/08/12.
+//  Copyright (c) 2012 Rafael Brigagão Paulino. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface classesTests : SenTestCase
+
+@end
